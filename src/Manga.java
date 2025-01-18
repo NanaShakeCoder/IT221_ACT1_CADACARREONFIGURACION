8cd7569a1bb6b5326e68938a596e53ec615e2266
NanaShakeCoder <2245241@slu.edu.ph>
2025-01-18 19:57:17 +0800

    Added Manga and Methods classes

---
+++ src/Manga.java
@@ -0,0 +1,17 @@
+public class Manga {
+    private String status;
+    private String title;
+    private String demographics;
+    private int score;
+    private int members;
+    private int scorers;
+    private int volumes;
+    private int favorites;
+    private int row;
+    private int column;
+
+    public Manga (){
+
+    }
+
+}

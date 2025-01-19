d4d98132da91aa5af6ab5f1d5e101bb1e17dd134
NanaShakeCoder <2245241@slu.edu.ph>
2025-01-19 13:32:46 +0800

    removed Methods class

--- src/Manga.java
+++ src/Manga.java
@@ -1,17 +1,45 @@
 public class Manga {
-    private String status;
     private String title;
+    private String status;
     private String demographics;
     private int score;
     private int members;
-    private int scorers;
-    private int volumes;
+    private int votes;
+    private int popularity;
     private int favorites;
-    private int row;
-    private int column;
 
     public Manga (){
-
+        this.title = "";
+        this.status = "";
+        this.demographics = "";
+        this.score = 0;
+        this.members = 0;
+        this.votes = 0;
+        this.popularity = 0;
+        this.favorites = 0;
     }
-
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public void setDemographics(String demographics) {
+        this.demographics = demographics;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void setMembers(int members) {
+        this.members = members;
+    }
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+    public void setPopularity(int popularity) {
+            this.popularity = popularity;
+    }
+    public void setFavorites(int favorites) {
+        this.favorites = favorites;
+    }
 }
